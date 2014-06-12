ea61430d19e25c9a37a3ee1f5d5e53213244ed05
Gibil <jrevilla55@gmail.com>
2014-06-12 09:56:45 -0500

    initial commit

---
+++ dreams_1.rb
@@ -0,0 +1,55 @@
+
+# Dreams 
+#--------
+#10 de junio 2014
+
+# Escrito apenas despierto, sin permitir que me invadan otros pensamientos
+
+Estoy en la ópera de Paris, que está construída como una espiral
+que va subiendo hacia arriba. Poco a poco. Llego tarde y eso hace que los 
+guardias me encaminen hacia un asiento que está arriba, pero un poco
+alejado del escenario. Me tengo que sentar en un lugar donde hay muchos niños.
+
+Pienso que mis amigos están en algún lugar más abajo, y más cerca del 
+escenario, con más espacio. Salgo para buscarlos. Pero luego me olvido. 
+Mientras tanto el concierto comienza y termina. Me lo pierdo. 
+No se me ocurre llamar por teléfono a mis amigos. Como que esa alternativa no existe. 
+
+Regreso pero el concierto ya terminó. Ocupo el asiento que me habían asignado
+y llego a ver a un violoncelista que está ensayando. El ensayo tan solo 
+me parece como música de los dioses. Comprendo que me he perdido algo significativo. 
+
+Mientras tanto comienzo a pensar en mi regreso. Hay un conflicto misterioso con 
+mi pasaje o con mi visa y tengo que volver a pagar dinero. Me estoy regresando 
+al Perú, con todas mis cosas, pero antes tengo que regresar a mi lugar de origen, 
+en Francia. Hay un conflicto. 
+
+Se me ocurre hacer algunas compras para mi familia. 
+Veo unos pequeños cuadros abstractos muy expresivos, con manchas rojas 
+y azules, sobre fondo blanco. Pienso que a Alicia le van a parecer demasiado
+simples. Luego encuentro unos objetos pequeños de acero de muy buena calidad. 
+El tipo de cosas que no hay forma de encontrar en mi país. 
+Son como unos garfios, o como unas mandíbulas, como pequenhas palas mecánicas, 
+con dientes y bordes afilados. 
+Tengo ganas de llevarme varios. Pero al final no compro nada. Me quedo con una sensación
+de desasosiego. 
+
+En algún momento, me encuentro sobre una colina, pero no se ve la ciudad. 
+Me cruzo con personas que están caminando hacia las montañas. Recuerdo a un 
+hombre alto y fornido, de cabello negro. Se parece a un Tarzán de los años cincuenta. 
+Pero con ropa alpina. Recuerdo su rostro con claridad. Está sonriendo benévolamente. 
+Se parece a una versión joven del actor alemán que hizo "el gato" y "los últimos días de hitler"
+
+En ese momento regreso a la ópera y cuando quiero salir la gente ha comenzado ya a salir también. 
+Comienzan a amontonarse y yo bajo rápido, porque sino voy a perder mi tren. 
+
+Cuando llego a la plataforma ya es demasiado tarde. Pero al mismo tiempo todavía no ha pasado. 
+Me doy cuenta que la policía ha puesto un comentario en la extensión de mi visa. 
+Algo como podría aprender mejor el francés. Me pongo furioso, porque en realidad 
+hablo muy bien el francés. 
+
+En ese momento veo unas ruinas muy hermosas y muy antiguas. De la época romana. 
+Han sido reconstruídas y se han caído varias veces. Veo otros monumentos hermosos
+en París. El sueño se termina. Es un atardecer con luces laterales y hermosas, que 
+afilan los contrastes de los muros y los edificios. 
+
---
+++ dreams_3.rb
@@ -0,0 +1,40 @@
+
+# Dreams
+# Trying to get clean in a dirty place
+
+12 de junio 2014
+
+
+Estoy en un gimnasio, oscuro, con paredes frías y casilleros de metal. 
+Estoy haciendo pesas. Mancuernas exactamente. Primero un brazo, después el otro. 
+Pienso que es bueno que haga ese tipo de ejercicio, porque mi cuerpo lo necesita. 
+Creo que tengo un polo manga zero, y veo mis músculos incipientes. 
+
+Luego llega la noche y tengo que dormir. Es un hotel, tipo ibis. 
+Resulta que hay una mujer durmiendo en mi cuarto. 
+Una desconocida. La luz está oscura y de pronto me dice "quieres hacer el amor ?" en francés. 
+Yo dudo porque es hermosa, blanca de piel pálida pero con facciones argelinas. 
+Pero tiene el cuerpo como cansado y fláccido, como después de muchos sufrimientos y partos. 
+Tengo temor de contagiarme, pero luego me doy cuenta de que tengo un preservativo y acepto. 
+Ella me dice "que quieres que te haga ?". Entonces, adivinando mis deseos, se da vuelta y me ofrece su cuerpo. 
+Yo me doy cuenta que tiene una cicatriz reciente, todavía roja, sobre el muslo izquierdo. 
+Parece una operación. Entonces, dudo y no consigo penetrarla. 
+
+Luego, al día siguiente la mujer se ha ido. Ha sido un mal sueño me digo. Menos mal que no eyaculé. 
+De pronto me doy cuenta que tengo el pantalón de pijama verde completamente mojado. Comprendo que he tenido 
+un sueño húmedo. Pero siento alivio de que fue tan solo un sueño. 
+
+En ese momento me encuentro con un amigo, con el cabello rubio teñido, que me dice que sabe que estoy buscando 
+formas de ganar dinero. 
+Me dice que trabaja en un prostíbulo masculino y que le pagan 30 euros por sesión. 
+Me muestra su tarjeta, que tiene caracteres dorados, como chinos. Me parece bastante dinero. 
+Entonces, yo le pregunto "Cómo haces si no consigues tener una erección ?" 
+El me dice que tiene derecho a rechazar de vez en cuando. 
+
+Toda esa conversación hace que me sienta muy sucio. Busco un lugar para ducharme. 
+Encuentro un baño público, en una estación de trenes en París. Está muy sucio, 
+como cubierto por una pátina de sarro y calcáreo. Por fin encuentro la ducha, al fondo a la derecha. 
+Quiero cerrar la puerta pero no lo consigo. No hay pestillo. Todo está muy sucio y no consigo bañarme...
+Tengo la sensación de que, en cualquier momento, puede entrar una persona. 
+Siento ansiedad. 
+
