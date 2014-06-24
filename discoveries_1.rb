b2dfb062c867582f85d7ad8e90c5be79b7e63a22
Gibil <jrevilla55@gmail.com>
2014-06-24 13:54:16 -0500

    discoveries 1

---
+++ discoveries_1.rb
@@ -0,0 +1,129 @@
+
+
+# Discoveries 1
+# ---------------
+# 24 junio 2014
+# Lima
+# (para que la historia no se siga repitiendo)
+
+
+
+Antes de que yo naciera sucedió algo muy importante. 
+Mamá tuvo un aborto espontáneo. 
+Luego yo nací y, de alguna manera, mamá volcó todas sus expectativas, su esperanza y su dolor sobre mí. 
+
+Luego, cuando tenía dos años sucedió otra cosa importante. 
+Mamá se fue a estados unidos para acompañar a papá que había ganado una beca para hacer un postgrado
+de medicina, en Nueva York. En ese momento tan crítico para el desarrollo de un niño, entonces, 
+nos abandonó. Nos dejó en casa de los abuelos en realidad, en el campo. 
+
+Quizá en ese momento, yo descubrí que mi conexión con mi abuela Mechita era más fuerte que la conexión 
+que había experimentado con Mamá. Ella siempre me ha dicho que yo soy Olazábal. 
+Mientras que ella se siente bastante Rondón. 
+Y así es, porque mi abuelo fue en su juventud y madurez un hombre bastante 
+duro y violento y estricto, sobre todo con sus hijos. Luego, con el tiempo, se ablandó, 
+pero quedó en el ambiente la sombra permanente de sus hazañas y su mal carácter. 
+
+Yo descubrí entonces que mi carácter era más como el de mi abuela. O sea, suave, dulce y sutil. 
+Quizás hasta bastante refinado. Mientras que mi madre, como mi abuelo, era campechana, fuerte y 
+bastante activa. 
+
+Quizás viví entonces tres meses de paz y tranquilidad. Reconocido por fin, completamente, por un adulto. 
+
+Entonces, sucedió algo que iba a cambiar mi vida de manera radical y para siempre. 
+Mi tío Mario que estaba regresando de estados unidos, manejando su camioneta (!), 
+tuvo un accidente en Méjico, Oaxaca. Mi madre se desmayó cuando se enteró y regreso inmediatamente a Lima. 
+Entonces, yo viví mi segunda pérdida y abandono, cuando tuve que dejar la casa de mi abuela, 
+por la casa incierta de mi madre. 
+
+Pero más importante aún, e irreversible, mi abuela cambió para siempre. 
+Después de la muerte de mi tío, perdió todo interés por la vida y el placer. 
+Se redujo a una pequeña expresión de lo que era. Le construyó un altar a su hijo muerto, 
+y se dedicó a esperar a que llegara su propia muerte. 
+
+Yo, quizás, de alguna manera, intenté sacarla de su tristeza, copiando el carácter del tío Mario. 
+Para reducir el dolor de la pérdida. 
+Me convertí así en un niño entusiasta, amiguero y emprendedor, divido siempre entre dos o más mujeres. 
+Dando amor de alguna manera por compasión. 
+
+También maté dos pájaros de un tiro, pues mi madre sentía adoración por mi tío. 
+Me aseguré así de su apoyo y su soporte, en terrenos tan inestables como son los primeros años 
+de un niño. Todos los años de un niño, en realidad.
+
+Fue así como me construí sobre dos muertos, y cerré así las heridas abiertas por la pérdida de dos madres. 
+
+
+
+Más tarde, en París, construí mi relación con Coralie sobre una muerta, su hermana. 
+Que había muerto de sobre-dosis (accidental ?) en California. Brillante, pero con una incapacidad
+congénita para la inserción social, Arianne murío ahogada en su propio vómito, después de casarse
+con Burt, productor de cine y millonario, probablemente con la edad de su padre. 
+Ella quería ser actriz, pero tenía un talento extraordinario para la pintura y la escultura. 
+Yo sentí seguramente esa herida y me dediqué a reconstruirla. 
+Con mi amor y dedicación, pero con un comportamiento irascible, de vez en cuando, iba yo reconstruyendo 
+el agujero dejado por la pérdida. Eso me aseguró una relación muy fuerte, pero inevitablemente
+compleja, donde el erotismo se mezclaba con el amor entre dos hermanas. 
+
+Eventualmente, la relación se convirtío en algo demasiado pesado y la perdí. 
+Sobre todo, porque se enteró de que le fui infiel con una buena amiga suya, cuando se fue a Egipto. 
+En un período en el que estuvimos separados. 
+
+
+
+Lo mismo me sucedió más de diez años después, en Lima. 
+Solo que, esta vez, el muerto era el hermano mayor de Alicia, que la había violentado cuando era pequeña, 
+y quizá violado. Brillante y con una capacidad congénita para la inserción social, murió de 
+arrepentimiento quizá, después de abandonar sus angustias al alcoholismo. 
+Alicia lo sobrevivió y lo quizo hasta el final, a pesar de su violencia y sus abusos. 
+
+También yo sentí esa herida y quize reconstruirla. 
+Asegurándome una vez más una relación muy fuerte y complicada. 
+Que eventualmente se convirtió en algo demasiado pesado para mí. 
+También le fui infiel con una amiga suya y también se enteró. 
+
+En ambos casos, yo terminé siendo la persona que estaba remplazando. 
+Y terminé limitando la relación. Sobre todo llevándola al territorio de lo patológico. 
+En ambos casos una relación muy fuerte y pasional, con menos sexo del que hubiera podido 
+haber. 
+
+En ambos casos, había un padre profundamente ausente y pueril. 
+Accidentado y alcoholizado, en el caso de Alicia. 
+Burócrata y buen burgués, en el caso de Coralie. 
+
+
+En el caso de Camila y Elise no hubo muertos. Pero si un padre profundamente ausente y pueril. 
+Ricachón y mujeriego, en el caso de Camila. 
+Campestre y alcohólico, en el caso de Elise. 
+
+
+
+A las cuatro las perdí con mucho y dolor y las sigo pensando. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
