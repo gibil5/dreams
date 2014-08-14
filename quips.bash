eaf0487be44039b9e93a3ac2070002420654e5e8
Gibil <jrevilla55@gmail.com>
2014-08-14 10:12:59 -0500

    new quips

---
+++ quips.bash
@@ -0,0 +1,11 @@
+
+echo ""
+echo "The construction of boundaries."
+echo "Grow a different skin, even if it has several layers." 
+echo "You will still have the time to change..."
+echo "However, begin from the inside. And, always, try to finish as time goes by."
+echo ""
+echo ""
+echo "Hurry up. Time goes by... Tomorrow, you will have one day less."
+echo ""
+
